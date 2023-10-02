65362ad87a6c7f8a5a15eb2d4992d9f3d5daf930
ITzombietux <zombietux@naver.com>
2023-10-02 22:05:42 +0900

    feat: APIClient Inferface 추가

--- RangomUSerListPackage/Sources/APIDependency/Inferface/APIClient.swift
+++ RangomUSerListPackage/Sources/APIDependency/Inferface/APIClient.swift
@@ -1,8 +1,58 @@
 //
-//  File.swift
+//  APIClient.swift
 //  
 //
 //  Created by 10004 on 2023/10/02.
 //
 
 import Foundation
+import Dependencies
+import XCTestDynamicOverlay
+import Moya
+import Model
+
+//MARK: - Moya API
+public enum RandomUserAPI {
+    case getRandomUser
+}
+
+extension RandomUserAPI: TargetType {
+    public var sampleData: Data {
+        return Data()
+    }
+    
+    public var baseURL: URL {
+        return URL(string: "https://randomuser.me")!
+    }
+    
+    public var path: String {
+        return "/api"
+    }
+    
+    public var method: Moya.Method {
+        return .get
+    }
+    
+    public var task: Moya.Task {
+        return .requestParameters(
+            parameters: ["results" : "20",
+                         "inc" : "name,enail,picture,male,login"],
+            encoding: URLEncoding.queryString)
+    }
+    
+    public var headers: [String : String]? {
+        return ["Accept": "application/json"]
+    }
+}
+
+//MARK: - Inferface
+public struct APIClient {
+    public var getRandomUsers: @Sendable () async throws -> RandomUsersResponse
+}
+
+extension DependencyValues {
+    public var apiClient: APIClient {
+        get { self[APIClient.self] }
+        set { self[APIClient.self] = newValue }
+    }
+}

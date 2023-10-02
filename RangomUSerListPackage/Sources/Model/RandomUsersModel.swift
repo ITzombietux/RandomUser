f31271039b5b4706e5d1caa4b7566bf414f613b4
ITzombietux <zombietux@naver.com>
2023-10-02 22:42:23 +0900

    fixed: Model 이니셜라이저 수정

--- RangomUSerListPackage/Sources/Model/RandomUsersModel.swift
+++ RangomUSerListPackage/Sources/Model/RandomUsersModel.swift
@@ -10,6 +10,12 @@ import Foundation
 public struct RandomUsersResponse: Codable {
     public let results: ResultsResponse
     public let info: InfoResponse
+    
+    public init(results: ResultsResponse = ResultsResponse(),
+                info: InfoResponse = InfoResponse()) {
+        self.results = results
+        self.info = info
+    }
 }
 
 public struct ResultsResponse: Codable {
@@ -17,23 +23,50 @@ public struct ResultsResponse: Codable {
     public let email: String
     public let login: LoginResponse
     public let picture: PictureResponse
+    
+    public init(name: NameResponse = NameResponse(),
+                email: String = "",
+                login: LoginResponse = LoginResponse(),
+                picture: PictureResponse = PictureResponse()) {
+        self.name = name
+        self.email = email
+        self.login = login
+        self.picture = picture
+    }
 }
 
 public struct NameResponse: Codable {
     public let title: String
     public let first: String
     public let last: String
+    
+    public init(title: String = "", first: String = "", last: String = "") {
+        self.title = title
+        self.first = first
+        self.last = last
+    }
 }
 
 public struct LoginResponse: Codable {
     public let uuid: String
     public let username: String
+    
+    public init(uuid: String = "", username: String = "") {
+        self.uuid = uuid
+        self.username = username
+    }
 }
 
 public struct PictureResponse: Codable {
     public let large: String
     public let medium: String
     public let thumbnail: String
+    
+    public init(large: String = "", medium: String = "", thumbnail: String = "") {
+        self.large = large
+        self.medium = medium
+        self.thumbnail = thumbnail
+    }
 }
 
 public struct InfoResponse: Codable {
@@ -41,4 +74,11 @@ public struct InfoResponse: Codable {
     let results: Int
     public let page: Int
     let version: String
+    
+    public init(seed: String = "", results: Int = 1, page: Int = 1, version: String = "") {
+        self.seed = seed
+        self.results = results
+        self.page = page
+        self.version = version
+    }
 }

796195ba98769e55072ae897d8340b65494fa4da
ITzombietux <zombietux@naver.com>
2023-10-02 22:05:54 +0900

    feat: APIClient Live 추가

--- RangomUSerListPackage/Sources/APIDependency/Live/APIClientLive.swift
+++ RangomUSerListPackage/Sources/APIDependency/Live/APIClientLive.swift
@@ -1,8 +1,36 @@
 //
-//  File.swift
+//  APIClientLive.swift
 //  
 //
 //  Created by 10004 on 2023/10/02.
 //
 
 import Foundation
+import Dependencies
+import Moya
+import Model
+import Core
+
+extension APIClient: DependencyKey {
+    public static let liveValue = Self(
+        getRandomUsers: {
+            let provider = MoyaProvider<RandomUserAPI>()
+            var randomUsers: RandomUsersResponse?
+            
+            provider.request(.getRandomUser) { result in
+                switch result {
+                case let .success(response) :
+                    guard let randomUsersResponse = try? response.map(RandomUsersResponse.self) else { return }
+                    
+                    randomUsers = randomUsersResponse
+                case let .failure(error):
+                    return
+                }
+            }
+            
+            try await NetworkTask.sleep()
+            return randomUsers
+        }
+    )
+}
+

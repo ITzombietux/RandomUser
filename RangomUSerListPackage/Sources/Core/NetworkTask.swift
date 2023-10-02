ab97a5c7c3fc711c659db54f0bfba5e039b35d20
ITzombietux <zombietux@naver.com>
2023-10-02 22:05:19 +0900

    feat: Task sleep static func 추가

--- RangomUSerListPackage/Sources/Core/NetworkTask.swift
+++ RangomUSerListPackage/Sources/Core/NetworkTask.swift
@@ -1,8 +1,15 @@
 //
-//  File.swift
+//  NetworkTask.swift
 //  
 //
 //  Created by 10004 on 2023/10/02.
 //
 
 import Foundation
+
+public struct NetworkTask {
+    public static func sleep() async throws {
+        try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+    }
+}
+

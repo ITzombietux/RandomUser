61bf4dcc252d6c564c2aa29fc88ec6e4a41b8162
ITzombietux <zombietux@naver.com>
2023-10-02 13:47:28 +0900

    feat: SPM Feature 파일 추가

---
+++ RandomUserList/RandomUserList/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  RandomUserList
+//
+//  Created by 10004 on 2023/10/02.
+//
+
+import Foundation
---
+++ RandomUserList/RandomUserList/App/RandomUserListApp.swift
@@ -0,0 +1,14 @@
+//
+//  RandomUserListApp.swift
+//  RandomUserList
+//
+//  Created by 10004 on 2023/10/02.
+//
+
+import SwiftUI
+
+struct RandomUserListApp: View {
+    var body: some View {
+        RootView()
+    }
+}

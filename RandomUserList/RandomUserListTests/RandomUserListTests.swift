ab5194064fa03d51ea9bb13d8c28c72079815287
ITzombietux <zombietux@naver.com>
2023-10-03 16:15:17 +0900

    feat: 중요 로직 테스트 코드 추가

--- RandomUserList/RandomUserListTests/RandomUserListTests.swift
+++ RandomUserList/RandomUserListTests/RandomUserListTests.swift
@@ -6,31 +6,53 @@
 //
 
 import XCTest
-@testable import RandomUserList
 
+import RandomUsers
+import APIDependency
+import Dependencies
+import Model
+
+@MainActor
 final class RandomUserListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testDeleteFeamaleGenderRandomUsers() async throws {
+        let model = withDependencies {
+            $0.apiClient = .onAppearMock()
+        } operation: {
+            RandomUsersViewModel()
         }
+        
+        await model.onAppear()
+        
+        XCTAssertEqual(model.randomUsers.count, 4)
     }
-
+    
+    func testDeleteDuplicatedRandomUsers() async throws {
+        let model = withDependencies {
+            $0.apiClient = .moreLoadMock()
+        } operation: {
+            RandomUsersViewModel()
+        }
+        
+        model.randomUsers = [ ResultsResponse(gender: "male", name: NameResponse(title: "mansa1"), login: LoginResponse(uuid: "AAAA-BBBB-1"))]
+        
+        await model.moreLoad()
+        
+        XCTAssertEqual(model.randomUsers.count, 4)
+    }
+    
+    func testRefreshRandomUsers() async throws {
+        let model = withDependencies {
+            $0.apiClient = .refreshMock()
+        } operation: {
+            RandomUsersViewModel()
+        }
+        
+        model.randomUsers = [
+            ResultsResponse(gender: "male", name: NameResponse(title: "mansa15"), login: LoginResponse(uuid: "AAAA-BBBB-15")),
+            ResultsResponse(gender: "male", name: NameResponse(title: "mansa16"), login: LoginResponse(uuid: "AAAA-BBBB-16"))]
+        
+        await model.onRefresh()
+        
+        XCTAssertEqual(model.randomUsers.count, 4)
+    }
 }
--- RangomUSerListPackage/Sources/APIDependency/Test/APIClientTest.swift
+++ RangomUSerListPackage/Sources/APIDependency/Test/APIClientTest.swift
@@ -7,9 +7,57 @@
 
 import Dependencies
 import XCTestDynamicOverlay
+import Model
 
 extension APIClient: TestDependencyKey {
     public static let testValue = Self(
         getRandomUsers: unimplemented("\(Self.self).getRandomUsers")
     )
+}
+
+extension APIClient {
+    public static func onAppearMock() -> APIClient {
+        return APIClient(
+            getRandomUsers: {
+                return [
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa1"), login: LoginResponse(uuid: "AAAA-BBBB-1")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa2"), login: LoginResponse(uuid: "AAAA-BBBB-2")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa3"), login: LoginResponse(uuid: "AAAA-BBBB-3")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa4"), login: LoginResponse(uuid: "AAAA-BBBB-4")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa5"), login: LoginResponse(uuid: "AAAA-BBBB-5")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa6"), login: LoginResponse(uuid: "AAAA-BBBB-6"))
+                ].filter { $0.gender == "male" }
+            }
+        )
+    }
+    
+    public static func moreLoadMock() -> APIClient {
+        return APIClient(
+            getRandomUsers: {
+                return [
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa1"), login: LoginResponse(uuid: "AAAA-BBBB-1")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa7"), login: LoginResponse(uuid: "AAAA-BBBB-7")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa8"), login: LoginResponse(uuid: "AAAA-BBBB-8")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa9"), login: LoginResponse(uuid: "AAAA-BBBB-9")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa10"), login: LoginResponse(uuid: "AAAA-BBBB-10")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa11"), login: LoginResponse(uuid: "AAAA-BBBB-11"))
+                ].filter { $0.gender == "male" }
+            }
+        )
+    }
+    
+    public static func refreshMock() -> APIClient {
+        return APIClient(
+            getRandomUsers: {
+                return [
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa1"), login: LoginResponse(uuid: "AAAA-BBBB-1")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa7"), login: LoginResponse(uuid: "AAAA-BBBB-7")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa8"), login: LoginResponse(uuid: "AAAA-BBBB-8")),
+                    ResultsResponse(gender: "female", name: NameResponse(title: "mansa9"), login: LoginResponse(uuid: "AAAA-BBBB-9")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa10"), login: LoginResponse(uuid: "AAAA-BBBB-10")),
+                    ResultsResponse(gender: "male", name: NameResponse(title: "mansa11"), login: LoginResponse(uuid: "AAAA-BBBB-11"))
+                ].filter { $0.gender == "male" }
+            }
+        )
+    }
 }
